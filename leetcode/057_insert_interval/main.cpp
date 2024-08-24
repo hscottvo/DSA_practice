3bd2cb26a32fd8f8478a7c3892978df9d10f0d4c
hscottvo <56327086+hscottvo@users.noreply.github.com>
2024-08-23 20:28:18 -0700

    Add correct implementation of insert interval

--- leetcode/057_insert_interval/main.cpp
+++ leetcode/057_insert_interval/main.cpp
@@ -1,74 +1,65 @@
-#include <complex>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 vector<vector<int>> insert(vector<vector<int>> &intervals,
                            vector<int> &newInterval) {
-  int start = newInterval.at(0);
-  int end = newInterval.at(1);
-
-  if (intervals.size() == 0) {
+  /*empty input*/
+  vector<vector<int>> ret;
+  if (intervals.size() == 0)
     return {newInterval};
-  } else if (newInterval.at(0) > intervals.at(intervals.size() - 1).at(1)) {
-    auto ret = intervals;
+  /*interval is left of input*/
+  if (intervals.at(0).at(0) > newInterval.at(1)) {
+    ret.push_back(newInterval);
+    for (auto i : intervals)
+      ret.push_back(i);
+    return ret;
+  }
+  /*interval is right of input*/
+  if (intervals.at(intervals.size() - 1).at(1) < newInterval.at(0)) {
+    for (auto i : intervals)
+      ret.push_back(i);
     ret.push_back(newInterval);
     return ret;
-  } else if (newInterval.at(1) < intervals.at(0).at(0)) {
-    vector<vector<int>> ret = {newInterval};
-    for (auto j : intervals) {
-      ret.push_back(j);
-    }
-    return ret;
   }
 
-  int i;
-
-  // get the first index that merges w the new interval
-  for (i = 0; i < intervals.size(); i++) {
-    if (intervals.at(i).at(1) > newInterval.at(0)) {
-
+  int start;
+  // add anything that doesn't need to be merged, on the left side of the
+  // interval
+  for (start = 0; start < intervals.size(); start++) {
+    // if the current interval can be merged, break. otherwise just push
+    if (intervals.at(start).at(1) >= newInterval.at(0)) {
       break;
     }
+    ret.push_back(intervals.at(start));
   }
 
-  vector<vector<int>> ret;
-  int idx;
-
-  for (idx = 0; idx < i; idx++) {
-    ret.push_back(intervals.at(idx));
+  int new_start = min(newInterval.at(0), intervals.at(start).at(0));
+  int new_end;
+  if (newInterval.at(1) >= intervals.at(start).at(0)) {
+    new_end = max(newInterval.at(1), intervals.at(start).at(1));
+  } else {
+    new_end = newInterval.at(1);
   }
-  if (i == intervals.size()) {
-    return {
-        {intervals.at(0).at(0), max(intervals.at(0).at(1), newInterval.at(1))}};
-  }
-
-  int start_new_int = min(intervals.at(idx).at(0), newInterval.at(0));
-  int end_new_int = newInterval.at(1);
-  cout << "New: " << start_new_int << endl;
-  for (; idx < intervals.size(); idx++) {
-
-    if (intervals.at(idx).at(0) > newInterval.at(1)) {
-      /*ret.push_back({start_new_int, end_new_int});*/
+  for (; start < intervals.size(); start++) {
+    if (intervals.at(start).at(0) > newInterval.at(1)) {
       break;
-    } else {
-      end_new_int = max(end_new_int, intervals.at(idx).at(1));
     }
-  }
-  if (intervals.at(idx - 1).at(0) <= newInterval.at(1)) {
-    ret.push_back({start_new_int, end_new_int});
+    new_end = max(new_end, intervals.at(start).at(1));
   }
 
-  for (; idx < intervals.size(); idx++) {
-    ret.push_back(intervals.at(idx));
+  ret.push_back({new_start, new_end});
+
+  for (; start < intervals.size(); start++) {
+    ret.push_back(intervals.at(start));
   }
 
   return ret;
 }
 
 int main(int argc, char *argv[]) {
-  vector<vector<int>> intervals = {{1, 3}, {6, 9}};
-  vector<int> new_interval = {3, 6};
+  vector<vector<int>> intervals = {{3, 5}, {12, 15}};
+  vector<int> new_interval = {6, 6};
   auto x = insert(intervals, new_interval);
   for (auto i : x) {
     for (auto j : i) {
